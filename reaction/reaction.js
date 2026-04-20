c4d7cdde9dd35065465d7549722650cd577a3553
Maksimnikolov <104394989+Maksimnikolov@users.noreply.github.com>
2026-04-20 14:39:44 -0400

    Change box color and update click behavior

--- reaction/reaction.js
+++ reaction/reaction.js
@@ -1,15 +1,21 @@
 const box = document.getElementById("box");
+const result = document.getElementById("result");
+
+box.style.height="200px";
+box.style.background="#eee";
+box.style.borderRadius="12px";
+
 let start;
 
 setTimeout(()=>{
-  box.style.background="green";
+  box.style.background="#111";
   start = Date.now();
 }, Math.random()*3000+1000);
 
 box.onclick = ()=>{
-  if(box.style.background==="green"){
-    alert("Time: " + (Date.now()-start) + "ms");
+  if(box.style.background==="#111"){
+    result.innerText = (Date.now()-start)+" ms";
   } else {
-    alert("Too early!");
+    result.innerText = "Too early";
   }
 };
